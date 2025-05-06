23de81ee3d320ca7e52647360d34021b146e8378
HimeMirai <milay.princessdj@cec.edu.ph>
2025-05-06 23:07:37 +0800

    second commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,3 +11,7 @@ def index():
         'Subject: System Integration and Architecture 1 <br>'
         'Semi-Final Exam'
     )
+
+if __name__=='__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
